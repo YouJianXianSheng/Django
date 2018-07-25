5497003680f9564b190dfc20c322c2c207f514a3
admin <admin@163.com>
2018-07-25 17:32:40 +0800

    myApp_展示班级和学生信息页面

--- project/myApp/urls.py
+++ project/myApp/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
+	#路由至主页
     url(r'^$', views.Index),
 	url(r'^(\d+)/$',views.detail),
+	#路由至grades
+	url(r'^grades/$',views.grades),
+	#路由至students
+	url(r'^students/$',views.students),
 ]
--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Grades,Students
 
 # Create your views here.
 def Index(request):
@@ -7,3 +8,15 @@ def Index(request):
 
 def detail(request,num):
 	return HttpResponse("detail-%s"%num)
+	
+def grades(request):
+	#从模型中获取数据
+	gradesList = Grades.objects.all()
+	#将数据返回至grades.html
+	return render(request,"myApp/grades.html",{"grades":gradesList}) 
+
+def students(request):
+	#从模型中获取数据
+	studentsList = Students.objects.all()
+	#将数据返回至grades.html
+	return render(request,"myApp/students.html",{"students":studentsList}) 
